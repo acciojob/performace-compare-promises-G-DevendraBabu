f2c47def2ed33932070c011db57348456270ff9b
G Devendrababu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-03-30 18:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,22 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+// Function to fetch data from APIs using Promise.all() and Promise.any()
+async function fetchData() {
+  const startTimeAll = performance.now(); // Start time for Promise.all()
+  const allResults = await Promise.all(apiUrls.map(url => fetch(url)));
+  const endTimeAll = performance.now(); // End time for Promise.all()
+  const timeTakenAll = endTimeAll - startTimeAll; // Time taken for Promise.all()
 
+  const startTimeAny = performance.now(); // Start time for Promise.any()
+  const anyResult = await Promise.any(apiUrls.map(url => fetch(url)));
+  const endTimeAny = performance.now(); // End time for Promise.any()
+  const timeTakenAny = endTimeAny - startTimeAny; // Time taken for Promise.any()
+
+  // Display time taken for each method in the table
+  document.getElementById("output-all").textContent = timeTakenAll;
+  document.getElementById("output-any").textContent = timeTakenAny;
+}
+
+fetchData(); // Call the fetchData function to display the results in the table
 // You can write your code here
